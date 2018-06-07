8593ae14760404e651ea4765aebae21262ad17d5
peng051410 <peng051410@126.com>
2018-06-07 10:39:00 +0800

    make Beer to abstract

--- factory/src/cn/imcompany/Beer.java
+++ factory/src/cn/imcompany/Beer.java
@@ -4,9 +4,12 @@ package cn.imcompany;
  * Created by tomyli on 2018/5/27.
  * Github: https://github.com/peng051410
  */
-public interface Beer {
+public abstract class Beer {
 
-    String getName();
-    
+    public abstract String getName();
+
+    public void getFlag() {
+        System.out.println("i am flag");
+    }
 
 }
--- factory/src/cn/imcompany/QingDao.java
+++ factory/src/cn/imcompany/QingDao.java
@@ -4,7 +4,7 @@ package cn.imcompany;
  * Created by tomyli on 2018/5/28.
  * Github: https://github.com/peng051410
  */
-public class QingDao implements Beer {
+public class QingDao extends Beer {
 
     @Override
     public String getName() {
