c9dc8f5d2f898a737696eb6efdc571be18739254
peng051410 <peng051410@126.com>
2018-06-23 16:15:19 +0800

    template pattern use jdbc template

---
+++ template/src/cn/imcompany/template/domain/Member.java
@@ -0,0 +1,36 @@
+package cn.imcompany.template.domain;
+
+/**
+ * Created by tomyli on 2018/6/21.
+ * Github: https://github.com/peng051410
+ */
+public class Member {
+
+    private String name;
+    private String passwd;
+    private String nikeName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getNikeName() {
+        return nikeName;
+    }
+
+    public void setNikeName(String nikeName) {
+        this.nikeName = nikeName;
+    }
+}
